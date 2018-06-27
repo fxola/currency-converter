0277cc49742ff908a2ff6d340e4cc1b91d786fd9
Fola62 <Afolabiabass62@gmail.com>
2018-06-27 22:48:25 +0100

    completed first phase of the challenge

--- js/index.js
+++ js/index.js
@@ -1,36 +1,90 @@
 const button = document.getElementsByClassName('button')[0];
+
 const from = document.getElementById('select-from');
+
 const to = document.getElementById('select-to');
+
+const amount = document.getElementById('amount');
+
 let fragment = document.createDocumentFragment();
 
-const populateDropdown =  (data) => {
+const board = document.getElementsByClassName('display-board')[0];
+
+const populateDropdown =  (data) => 
+{
     const countryInfo = data.results;
-    for(const[key,value] of Object.entries(countryInfo)){
-        let option = document.createElement('option'); 
-            option.value = `${value.currencyId}`;
-            option.textContent = `${value.currencyName} (${value.currencySymbol})`;
-            fragment.appendChild(option);  
+
+    for(const[key,value] of Object.entries( countryInfo ) )
+    {
+
+        let option = document.createElement('option');
+
+        option.value = `${value.currencyId}`;
+
+        option.textContent = `${value.currencyName} (${value.currencySymbol})`;
+
+        fragment.appendChild(option);  
     }
+
     from.appendChild(fragment);
 }
 
-const populateDropdown2 =  (data) => {
+const populateDropdown2 =  (data) => 
+{
     const countryInfo = data.results;
-    for(const[key,value] of Object.entries(countryInfo)){
-        let option = document.createElement('option');    
+
+    for( const[key,value] of Object.entries(countryInfo) )
+    {
+
+        let option = document.createElement('option'); 
+
+        option.value = `${value.currencyId}`;
+
         option.textContent = `${value.currencyName} (${value.currencySymbol})`;
+
         fragment.appendChild(option);  
     }
+
     to.appendChild(fragment);
 }
 
-fetch('https://free.currencyconverterapi.com/api/v5/countries').then(function(data){
+fetch('https://free.currencyconverterapi.com/api/v5/countries').then( (data) =>
+{
+
     let response2 = data.clone();
+
     data.json().then(populateDropdown);
+
     response2.json().then(populateDropdown2);
 });
 
-button.addEventListener('click', function(e){
+button.addEventListener('click', (e) =>
+{
+
     e.preventDefault();
 
-})
+    convertFrom = from.value;
+
+    convertTo = to.value;
+
+    fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${convertFrom}_${convertTo}&compact=y`).then(function(data){
+
+        return data.json();
+
+    }).then(function(data){
+
+        let query = `${convertFrom}_${convertTo}`;
+
+        let exchangeValue = data[query].val;
+
+        let exchangeAmount = amount.value;
+        // console.log(exchangeAmount);
+        let exchangeRate = exchangeAmount*exchangeValue;
+        board.textContent = exchangeRate;
+
+    }).catch(function(err){
+
+        console.log(err);
+    });
+
+});
