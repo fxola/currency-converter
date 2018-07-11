16ae239936f778a64fd33694d249424c41227bf3
Fola62 <Afolabiabass62@gmail.com>
2018-07-11 19:35:34 +0100

    caching debug

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 self.addEventListener('install', (e)=>{
     e.waitUntil(
-        caches.open('static-resource-v2').then((cache)=>{
+        caches.open('static-resources').then((cache)=>{
             return cache.addAll([
-                './index.html',
-                './css/font/digital-7-italic.ttf',
-                './css/normalize.css',
-                './css/stylesheet.css',
+                'index.html',
+                'css/font/digital-7-italic.ttf',
+                'css/normalize.css',
+                'css/stylesheet.css',
                 'https://fonts.googleapis.com/css?family=Lato',
                 'https://free.currencyconverterapi.com/api/v5/countries'
             ])
